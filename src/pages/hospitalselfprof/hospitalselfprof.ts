ad7cf75939f67942ed67615107f439152be20c14
abmnukmr <developer.abmnu@gmail.com>
2017-09-28 02:03:27 +0530

    asdfkagsfk

--- src/pages/hospitalselfprof/hospitalselfprof.ts
+++ src/pages/hospitalselfprof/hospitalselfprof.ts
@@ -1,7 +1,8 @@
 import {Component, NgZone, ViewChild} from '@angular/core';
 import {AlertController, Content, IonicPage, ModalController, NavController, NavParams} from 'ionic-angular';
 import {HospieditPage} from "../hospiedit/hospiedit";
-
+import {Http} from "@angular/http";
+import  * as firebase from 'firebase'
 /**
  * Generated class for the HospitalselfprofPage page.
  *
@@ -14,10 +15,18 @@ import {HospieditPage} from "../hospiedit/hospiedit";
   templateUrl: 'hospitalselfprof.html',
 })
 export class HospitalselfprofPage {
-
+   data:any;
+   emailsearch:any;
   showheader:boolean=true;
   @ViewChild(Content) content: Content;
-  constructor(public Mdl:ModalController,public navCtrl: NavController, public navParams: NavParams, public zone:NgZone,public alertCtrl: AlertController) {
+  constructor(public http:Http,public Mdl:ModalController,public navCtrl: NavController, public navParams: NavParams, public zone:NgZone,public alertCtrl: AlertController) {
+    var user = firebase.auth().currentUser;
+    if (user != null) {
+      var  name = user.displayName;
+      this.emailsearch = user.email;
+      var  photoUrl = user.photoURL;
+    }
+  this.load(this.emailsearch);
     //this.detescroll();
 
   }
@@ -60,5 +69,46 @@ export class HospitalselfprofPage {
     let er=this.Mdl.create(HospieditPage)
     er.present()
   }
+
+
+  load(em)
+  {
+
+
+
+    if(this.data) {
+
+      return new Promise(resolve => {
+
+        this.http.get('https://quiet-ridge-46090.herokuapp.com/be/hospital/'+em).map(res => res.json()).subscribe(data => {
+          this.data = data
+          resolve(this.data);
+          // this.chemist=data
+          console.log(this.data)
+        });
+
+      });
+    }
+
+    return new Promise(resolve => {
+
+      this.http.get('https://quiet-ridge-46090.herokuapp.com/be/hospital/'+em).map(res => res.json()).subscribe(data => {
+
+        this.data = data
+        resolve(this.data);
+        // this.chemist=data;
+        console.log(this.data)
+
+      });
+
+    });
+
+
+
+  }
+
+
+
+
 
 }
